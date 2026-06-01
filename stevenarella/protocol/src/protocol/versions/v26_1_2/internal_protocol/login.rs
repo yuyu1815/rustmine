0e51176aa81155faf7c6da23c46b99b31af14c86
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-06-01 14:02:30 +0900

    Split login clientbound handling

--- stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/login.rs
+++ stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/login.rs
@@ -1,11 +1,10 @@
 use std::io;
 
-use crate::protocol::{
-    packet, read_lenient_json_component, Direction, Error, Serializable, State, VarInt, UUID,
-};
+use crate::protocol::{packet, Direction, Error, State};
 
 use super::super::translate_internal_packet_id;
 
+mod clientbound;
 mod serverbound;
 
 pub(crate) fn read_login_packet_by_id<R: io::Read>(
@@ -19,37 +18,10 @@ pub(crate) fn read_login_packet_by_id<R: io::Read>(
                 translate_internal_packet_id(State::Login, Direction::Serverbound, id, true);
             serverbound::read_login_serverbound_packet_by_internal_id(internal_id, buf)
         }
-        Direction::Clientbound => read_login_clientbound_packet(id, buf),
-    }
-}
-
-fn read_login_clientbound_packet<R: io::Read>(
-    id: i32,
-    buf: &mut R,
-) -> Result<Option<packet::Packet>, Error> {
-    let internal_id = translate_internal_packet_id(State::Login, Direction::Clientbound, id, true);
-    if internal_id == packet::login::clientbound::internal_ids::LoginDisconnect {
-        return Ok(Some(packet::Packet::LoginDisconnect(
-            packet::login::clientbound::LoginDisconnect {
-                reason: read_lenient_json_component(buf)?,
-            },
-        )));
-    }
-    if internal_id == packet::login::clientbound::internal_ids::LoginSuccess_UUID {
-        let uuid: UUID = Serializable::read_from(buf)?;
-        let username: String = Serializable::read_from(buf)?;
-        let property_count: VarInt = Serializable::read_from(buf)?;
-        for _ in 0..property_count.0 {
-            let _property_name: String = Serializable::read_from(buf)?;
-            let _property_value: String = Serializable::read_from(buf)?;
-            let has_signature: bool = Serializable::read_from(buf)?;
-            if has_signature {
-                let _property_signature: String = Serializable::read_from(buf)?;
-            }
+        Direction::Clientbound => {
+            let internal_id =
+                translate_internal_packet_id(State::Login, Direction::Clientbound, id, true);
+            clientbound::read_login_clientbound_packet_by_internal_id(internal_id, buf)
         }
-        return Ok(Some(packet::Packet::LoginSuccess_UUID(
-            packet::login::clientbound::LoginSuccess_UUID { uuid, username },
-        )));
     }
-    Ok(None)
 }
