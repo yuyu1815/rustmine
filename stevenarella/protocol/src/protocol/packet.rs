077bd19ca13f3496d47c206d186849033e58bf37
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-06-01 14:54:14 +0900

    Split packet movement helpers

--- stevenarella/protocol/src/protocol/packet.rs
+++ stevenarella/protocol/src/protocol/packet.rs
@@ -3159,7 +3159,6 @@ pub struct PlayerProperty {
 
 use crate::item;
 use crate::item::Stack;
-use std::f32::consts::PI;
 
 type RecipeIngredient = LenPrefixed<VarInt, Option<item::Stack>>;
 
@@ -3668,78 +3667,8 @@ impl Serializable for CommandNode {
 
 pub mod interaction;
 pub use interaction::{DigType, Hand};
-
-pub fn send_position_look(
-    conn: &mut Conn,
-    position: &Vector3<f64>,
-    yaw: f32,
-    pitch: f32,
-    on_ground: bool,
-) -> Result<(), Error> {
-    let version = conn.get_version();
-    if version >= Version::V1_8 {
-        conn.write_packet(
-            crate::protocol::packet::play::serverbound::PlayerPositionLook {
-                x: position.x,
-                y: position.y,
-                z: position.z,
-                yaw: -yaw * (180.0 / PI),
-                pitch: (-pitch * (180.0 / PI) + 180.0).min(90.0), // used to make sure, that we don't send impossible pitch values
-                on_ground,
-            },
-        )
-    } else {
-        conn.write_packet(
-            crate::protocol::packet::play::serverbound::PlayerPositionLook_HeadY {
-                x: position.x,
-                feet_y: position.y,
-                head_y: position.y + 1.62,
-                z: position.z,
-                yaw: -yaw * (180.0 / PI),
-                pitch: (-pitch * (180.0 / PI) + 180.0).min(90.0), // used to make sure, that we don't send impossible pitch values
-                on_ground,
-            },
-        )
-    }
-}
-
-pub fn send_position(
-    conn: &mut Conn,
-    position: &Vector3<f64>,
-    on_ground: bool,
-) -> Result<(), Error> {
-    let version = conn.get_version();
-    if version >= Version::V1_8 {
-        conn.write_packet(crate::protocol::packet::play::serverbound::PlayerPosition {
-            x: position.x,
-            y: position.y,
-            z: position.z,
-            on_ground,
-        })
-    } else {
-        conn.write_packet(
-            crate::protocol::packet::play::serverbound::PlayerPosition_HeadY {
-                x: position.x,
-                z: position.z,
-                on_ground,
-                feet_y: position.y,
-                head_y: position.y + 1.62,
-            },
-        )
-    }
-}
-
-pub fn send_look(conn: &mut Conn, yaw: f32, pitch: f32, on_ground: bool) -> Result<(), Error> {
-    conn.write_packet(crate::protocol::packet::play::serverbound::PlayerLook {
-        yaw: -yaw * (180.0 / PI),
-        pitch: (-pitch * (180.0 / PI) + 180.0).min(90.0), // used to make sure, that we don't send impossible pitch values
-        on_ground,
-    })
-}
-
-pub fn send_flying(conn: &mut Conn, on_ground: bool) -> Result<(), Error> {
-    conn.write_packet(crate::protocol::packet::play::serverbound::Player { on_ground })
-}
+pub mod movement;
+pub use movement::{send_flying, send_look, send_position, send_position_look};
 
 pub fn send_client_status(conn: &mut Conn, status: ClientStatus) -> Result<(), Error> {
     let version = conn.get_version();
