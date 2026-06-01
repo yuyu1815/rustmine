4d9b6c49b2bdfa344f1136b1c02b9428fa0a9c7e
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-06-01 19:42:55 +0900

    Split configuration clientbound update tags packets

--- stevenarella/protocol/src/protocol/packet/configuration/clientbound/mod.rs
+++ stevenarella/protocol/src/protocol/packet/configuration/clientbound/mod.rs
@@ -2,9 +2,14 @@ use crate::protocol::*;
 use std::io;
 
 mod resource_pack;
+mod update_tags;
 
 pub use resource_pack::{
     ConfigurationResourcePackPopClientbound, ConfigurationResourcePackPushClientbound,
+};
+pub use update_tags::{
+    ConfigurationUpdateTagsClientbound, ConfigurationUpdateTagsRegistryPayload,
+    ConfigurationUpdateTagsTagPayload,
 };
 
 #[allow(non_upper_case_globals)]
@@ -214,51 +219,6 @@ impl PacketType for ConfigurationUpdateEnabledFeaturesClientbound {
         VarInt(self.features.len() as i32).write_to(buf)?;
         for feature in &self.features {
             feature.write_to(buf)?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Default, Debug)]
-pub struct ConfigurationUpdateTagsTagPayload {
-    pub tag_key: String,
-    pub entry_ids: Vec<i32>,
-}
-
-#[derive(Default, Debug)]
-pub struct ConfigurationUpdateTagsRegistryPayload {
-    pub registry_key: String,
-    pub tags: Vec<ConfigurationUpdateTagsTagPayload>,
-}
-
-#[derive(Default, Debug)]
-pub struct ConfigurationUpdateTagsClientbound {
-    pub registry_payloads: Vec<ConfigurationUpdateTagsRegistryPayload>,
-}
-
-impl PacketType for ConfigurationUpdateTagsClientbound {
-    fn packet_id(&self, version: i32) -> i32 {
-        packet::versions::translate_internal_packet_id_for_version(
-            version,
-            State::Configuration,
-            Direction::Clientbound,
-            internal_ids::ConfigurationUpdateTagsClientbound,
-            false,
-        )
-    }
-
-    fn write<W: io::Write>(&self, buf: &mut W) -> Result<(), Error> {
-        VarInt(self.registry_payloads.len() as i32).write_to(buf)?;
-        for registry in &self.registry_payloads {
-            registry.registry_key.write_to(buf)?;
-            VarInt(registry.tags.len() as i32).write_to(buf)?;
-            for tag in &registry.tags {
-                tag.tag_key.write_to(buf)?;
-                VarInt(tag.entry_ids.len() as i32).write_to(buf)?;
-                for entry_id in &tag.entry_ids {
-                    VarInt(*entry_id).write_to(buf)?;
-                }
-            }
         }
         Ok(())
     }
