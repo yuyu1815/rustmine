4a71ebef1f5f01a84457d6e3b163f8d22093d8d0
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-06-01 14:28:51 +0900

    Split play set equipment handling

--- stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/play_clientbound/entity.rs
+++ stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/play_clientbound/entity.rs
@@ -1,12 +1,10 @@
 use std::io;
 
-use crate::protocol::{
-    packet::{self, Packet},
-    Error, Serializable, VarInt,
-};
+use crate::protocol::{packet::Packet, Error};
 
 mod set_entity_data;
 mod set_entity_link;
+mod set_equipment;
 mod set_passengers;
 mod teleport_entity;
 
@@ -33,6 +31,11 @@ pub(crate) fn read_entity_clientbound_packet_by_internal_id<R: io::Read>(
         return Ok(Some(packet));
     }
     if let Some(packet) =
+        set_equipment::read_set_equipment_clientbound_packet_by_internal_id(internal_id, buf)?
+    {
+        return Ok(Some(packet));
+    }
+    if let Some(packet) =
         set_passengers::read_set_passengers_clientbound_packet_by_internal_id(internal_id, buf)?
     {
         return Ok(Some(packet));
@@ -43,25 +46,5 @@ pub(crate) fn read_entity_clientbound_packet_by_internal_id<R: io::Read>(
         return Ok(Some(packet));
     }
 
-    match internal_id {
-        packet::play::clientbound::internal_ids::PlaySetEquipmentClientbound => {
-            let entity_id = VarInt::read_from(buf)?;
-            let equipment_slot = u8::read_from(buf)?;
-            if equipment_slot & 0x80 != 0 {
-                return Err(Error::Err(format!(
-                    "unsupported multi-entry Play set_equipment slot byte {}",
-                    equipment_slot
-                )));
-            }
-            super::item_stack_marker::read_empty_play_item_stack_marker(buf, "set_equipment")?;
-            Ok(Some(Packet::PlaySetEquipmentClientbound(
-                packet::play::clientbound::PlaySetEquipmentClientbound {
-                    entity_id,
-                    equipment_slot,
-                    item: None,
-                },
-            )))
-        }
-        _ => Ok(None),
-    }
+    Ok(None)
 }
