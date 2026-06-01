545cdafc870c134490578be84e7c8b99fbe7f108
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2026-06-01 13:38:25 +0900

    Split play teleport entity handling

--- stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/play_clientbound/entity.rs
+++ stevenarella/protocol/src/protocol/versions/v26_1_2/internal_protocol/play_clientbound/entity.rs
@@ -1,13 +1,13 @@
 use std::io;
 
-use byteorder::{BigEndian, ReadBytesExt};
-
 use crate::protocol::{
     packet::{self, Packet},
     Error, LenPrefixed, Serializable, VarInt,
 };
 
 use super::read_empty_play_item_stack_marker;
+
+mod teleport_entity;
 
 pub(crate) fn read_entity_clientbound_packet_by_internal_id<R: io::Read>(
     internal_id: i32,
@@ -18,6 +18,11 @@ pub(crate) fn read_entity_clientbound_packet_by_internal_id<R: io::Read>(
             internal_id,
             buf,
         )?
+    {
+        return Ok(Some(packet));
+    }
+    if let Some(packet) =
+        teleport_entity::read_teleport_entity_clientbound_packet_by_internal_id(internal_id, buf)?
     {
         return Ok(Some(packet));
     }
@@ -87,39 +92,6 @@ pub(crate) fn read_entity_clientbound_packet_by_internal_id<R: io::Read>(
                 packet::play::clientbound::PlaySetPassengersClientbound {
                     vehicle_entity_id,
                     passenger_entity_ids,
-                },
-            )))
-        }
-        packet::play::clientbound::internal_ids::PlayTeleportEntityClientbound => {
-            let entity_id = VarInt::read_from(buf)?;
-            let position_x = buf.read_f64::<BigEndian>()?;
-            let position_y = buf.read_f64::<BigEndian>()?;
-            let position_z = buf.read_f64::<BigEndian>()?;
-            let delta_x = buf.read_f64::<BigEndian>()?;
-            let delta_y = buf.read_f64::<BigEndian>()?;
-            let delta_z = buf.read_f64::<BigEndian>()?;
-            let y_rot = buf.read_f32::<BigEndian>()?;
-            let x_rot = buf.read_f32::<BigEndian>()?;
-            let relative_mask = buf.read_i32::<BigEndian>()?;
-            if relative_mask != 0 {
-                return Err(Error::Err(format!(
-                    "unsupported Play teleport_entity non-empty relative mask {}",
-                    relative_mask
-                )));
-            }
-            Ok(Some(Packet::PlayTeleportEntityClientbound(
-                packet::play::clientbound::PlayTeleportEntityClientbound {
-                    entity_id,
-                    position_x,
-                    position_y,
-                    position_z,
-                    delta_x,
-                    delta_y,
-                    delta_z,
-                    y_rot,
-                    x_rot,
-                    relative_mask,
-                    on_ground: bool::read_from(buf)?,
                 },
             )))
         }
